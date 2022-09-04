cc49dba78008c3b69e3e7e7cb247050c0f5ffaf2
dantezare <108190305+dantezare@users.noreply.github.com>
2022-09-04 10:22:02 -0500

    Add files via upload

---
+++ mov.js
@@ -0,0 +1,16 @@
+alert("Holiiiii :3 \nSolo minusculas :0");
+respuesta1 = prompt("Como te llamo?");
+while (respuesta1 !== 'verito') {
+    alert("Respuesta incorrecta :v \nSigue intentando!!!");
+    respuesta1 = prompt("Como te llamo?");
+} 
+respuesta2 = prompt("Como me llamas?");
+while (respuesta2 !== 'dantecito') {
+    alert("Respuesta incorrecta :v \nSigue intentando!!!");
+    respuesta2 = prompt("Como me llamas?");
+} 
+respuesta3 = prompt("Donde te conocì?");
+while (respuesta3 !== 'valorant') {
+    alert("Me ofendes, peroooo :v \nSigue intentando aaaaaa!!!");
+    respuesta3 = prompt("Donde te conoci?");
+} 
